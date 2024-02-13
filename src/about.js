29a6a2300ea4a2c3937dc875636b81f0a75b220b
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-02-13 17:13:05 +0530

    Update about.js

--- src/about.js
+++ src/about.js
@@ -1,1 +1,47 @@
 
+import React from "react";
+class About extends React.Component {
+  constructor() {
+    super();
+    console.warn("props", this.props);
+
+    // this.state = {
+    //   name: "Rupesh",
+    //   age: 25,
+    // };
+    // console.warn("Constructor  ");
+  }
+  componentDidMount() {
+    console.warn("props", this.props);
+    console.warn("props", this.props.skill);
+
+    // console.warn("did mount");
+  }
+  componentDidUpdate() {
+    // console.warn("did Update    ");
+    // alert("Data Updated ");
+  }
+
+  // componentDidCatch(){}
+  // componentWillUnmount(){}
+  render() {
+    // console.warn("render");
+
+    return (
+      <div>
+        <h1>About us component</h1>
+        {/* <h2>{this.state.name}</h2> */}
+        {/* <h2>{this.state.age}</h2> */}
+        {/* <button
+          onClick={() => {
+            this.setState({ name: "rupesh bavaskar" });
+          }}
+        >
+          Update State
+        </button> */}
+      </div>
+    );
+  }
+}
+
+export default About;
